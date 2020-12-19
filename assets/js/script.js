6c8735be0c84a09b0f2a62ba2bc72f96e2807b3b
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-19 14:39:50 -0800

    quiz question div created with progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,113 @@
-var bodyHTML = document.querySelector(".start-page-div")
-
-//variables
-    //quizcontainer
-    //startSection
-    //questionsection
-    //resultssection
-    //counter
-    //timer
-    //time
-    //index
-    //score
-
-//array to hold the questions and answers
-var quizQuestions = [
-    {
-        question: "Commonly used data types DO NOT include: ",
-        answers: ["A. Strings", "B. Booleans", "C. Alerts", "D.Numbers"],
-        correctAnswer: "B. Booleans",
-        
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within ___.",
-        answers: ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"],
-        correctAnswer: "C. Parentheses",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ___.",
-        answers: ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"],
-        correctAnswer: "D. All of the Above",
-    },
-    {
-        question: "String values must be enclosed within ______ when being assigned to variables.",
-        answer: ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parantheses"],
-        correctAnswer: "C. Quotes",
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["A. JavaScript", "B. Terminal/Bash", "C. For Loops", "D. Console Log"],
-        correctAnswer: "D. Console Log",
+function quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
-    
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+
+var questions = [
+    new Question("Commonly used data types DO NOT include: ", ["A. Strings", "B. Booleans", "C. Alerts", "D.Numbers"], "B. Booleans"),
+    new Question("The condition in an if/else statement is enclosed within ___.", ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"], "C. Parentheses"),
+    new Question("Arrays in JavaScript can be used to store ___.", ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"], "D. All of the Above"),
+    new Question("String values must be enclosed within ______ when being assigned to variables.", ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parantheses"], "C. Quotes"),
+    new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["A. JavaScript", "B. Terminal/Bash", "C. For Loops", "D. Console Log"], "D. Console Log"),
 ]
 
-//startup page with directions
-//innerhtml deletes what was in the section, textContent adds to section 
-var startPage = function() {
-    var title= document.createElement("div");
-    title.className = "start-page";
-    title.innerHTML="<h1 class='title'>Javascript Challenge</h1>";
-    bodyHTML.appendChild(title);
 
-    var directions = document.createElement("div");
-    directions.className = "directions";
-    directions.innerHTML = "<h2 class='directions'>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</h2";
-    bodyHTML.appendChild(directions);
+//create quiz
+var quiz = new Quiz(questions);
 
-    var startButton = document.createElement("button");
-    startButton.className= "";
-    startButton.innerHTML = "<button class='button'>Start</button>";
-    bodyHTML.appendChild(startButton);
-}
+//display quiz
+populate();
+
+//on click, clear the old div and put in the question div
+
+// function clearBox(directions) {
+//     document.getElementById(directions).innerHTML = "";
 
 
-startPage();
+    
 
-// function to start quiz
-function startQuiz () {}
+ 
+
+
+//display quiz right away
+
+
+//on submit, show results
+
+
 //function to star ttimer
 
 //function to build the quiz
@@ -75,3 +117,4 @@ function startQuiz () {}
 //function of clicking on the button
 
 //function to show results
+
