52db8ff74cd0ef745f9aa4790fcdc5bec1e02525
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-18 20:58:27 -0800

    Created question/answer array

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,28 @@ var quizQuestions = [
         answers: ["A. Strings", "B. Booleans", "C. Alerts", "D.Numbers"],
         correctAnswer: "B. Booleans",
         
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        answers: ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"],
+        correctAnswer: "C. Parentheses",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        answers: ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"],
+        correctAnswer: "D. All of the Above",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answer: ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parantheses"],
+        correctAnswer: "C. Quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["A. JavaScript", "B. Terminal/Bash", "C. For Loops", "D. Console Log"],
+        correctAnswer: "D. Console Log",
     }
+    
 ]
 
 //startup page with directions
