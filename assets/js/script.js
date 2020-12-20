5db4e88a2ca336dce298669dc2769ccd9d8cce9c
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-19 20:05:44 -0800

    removed directions div to start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 // //display start page
 var masterContainer = document.createElement("div");
 masterContainer.className = "container";
+masterContainer.id = "id-container";
 
 var startPage = document.createElement("div");
 startPage.className = "start-page";
 startPage.innerHTML="<h1>JavaScript Challenge</h1>";
 masterContainer.appendChild(startPage);
 
- var directions = document.createElement("div");
-    directions.className = "directions";
-    directions.innerHTML="<h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</h2>";
-    masterContainer.appendChild(directions);
+var directions = document.createElement("div");
+directions.className = "directions";
+directions.innerHTML="<h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</h2>";
+masterContainer.appendChild(directions);
 
 var startButton = document.createElement("button");
 startButton.className = "container startButton";
-startButton.innerHTML = "Start"
+startButton.innerHTML = "Start Quiz"
 startButton.onclick = function() {startQuiz()}; 
 masterContainer.appendChild(startButton);
 
@@ -23,19 +24,12 @@ document.body.appendChild(masterContainer);
    
 
     
-
 function startQuiz() {
     console.log("This worked.");
-    
-
-    
+    var removeDirections = document.getElementById("id-container");
+    removeDirections.remove(); 
 }
     
-    
-// startPage();
-//on click, clear the old div and put in the question div
-    // function clearBox(directions) {
-    //document.getElementById(directions).innerHTML = "";
 
 //timer should change if they get the question wrong??
 
