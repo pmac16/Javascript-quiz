72ba01e56f653ba035b4c05f4b285057478912ae
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-20 09:38:06 -0800

    updated timer to countdown the entire time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+//Global Variables
+var timeLeft = 10;
+var timerEl = document.getElementById('countdown');
+
 // //display start page
 var masterContainer = document.createElement("div");
 masterContainer.className = "container";
@@ -21,30 +25,38 @@ masterContainer.appendChild(startButton);
 
 document.body.appendChild(masterContainer);
 
-   
-
-    
+//Remove the directions and start the quiz    
 function startQuiz() {
-    console.log("This worked.");
     var removeDirections = document.getElementById("id-container");
     removeDirections.remove(); 
+    questionProcessing();
 }
-    
 
-//timer should change if they get the question wrong??
+//Begin the timer 
+function questionProcessing() {
+    while (timeLeft > 0) 
+        for (let j = 0;j<questions.length;j++){
+        //show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        };    
+        }
+    }
 
-// var timerEl = document.getElementById('countdown');
 
-// //time that counts down from 50
+
 // function countdown() {
-//     var timeLeft = 50;
-
 //     var timeInterval = setInterval(function() {
-       
-        
-
 //         if (timeLeft >=1) {
-//             timerEl.textContent =timeLeft;
+//             var textOut = "Timer: ".concat(parseInt(timeLeft)); 
+//             timerEl.textContent = textOut;
 //             timeLeft--;
 //         }
 //         else if (timeLeft === 0) {
@@ -54,111 +66,104 @@ function startQuiz() {
 //     }, 1000);
 // }
 
-// countdown();
+// Quiz Div Section
 
-// // Quiz Div Section
-// function Quiz(questions) {
-//     this.score = 0;
-//     this.questions = questions;
-//     this.questionIndex = 0;
-// }
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
  
-// Quiz.prototype.getQuestionIndex = function() {
-//     return this.questions[this.questionIndex];
-// }
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
  
-// Quiz.prototype.guess = function(answer) {
-//     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-//         this.score++;
-//     }
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
  
-//     this.questionIndex++;
-// }
+    this.questionIndex++;
+}
  
-// Quiz.prototype.isEnded = function() {
-//     return this.questionIndex === this.questions.length;
-// }
- 
- 
-// function Question(text, choices, answer) {
-//     this.text = text;
-//     this.choices = choices;
-//     this.answer = answer;
-// }
- 
-// Question.prototype.isCorrectAnswer = function(choice) {
-//     return this.answer === choice;
-// }
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
  
  
-// function populate() {
-//     //change this to say that if thee quiz is ended, put in high score
-//     if(quiz.isEnded()) {
-//         showScores();
-//     }
-//     else {
-//         // show question
-//         var element = document.getElementById("question");
-//         element.innerHTML = quiz.getQuestionIndex().text;
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
  
-//         // show options
-//         var choices = quiz.getQuestionIndex().choices;
-//         for(var i = 0; i < choices.length; i++) {
-//             var element = document.getElementById("choice" + i);
-//             element.innerHTML = choices[i];
-//             guess("btn" + i, choices[i]);
-//         }
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
  
-//         showProgress();
-//     }
+ 
+function populate() {
+    //change this to say that if thee quiz is ended, put in high score
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+//change this to show if they got it correct or not 
+function questionCorrect() {
+    //  if answer is correct,
+    element.innerHTML = "Correct"
+    //if answer is incorrect, display incorrect and take away time
+}
+// function showProgress() {
+//     var currentQuestionNumber = quiz.questionIndex + 1;
+//     var element = document.getElementById("progress");
+//     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 // };
- 
-// function guess(id, guess) {
-//     var button = document.getElementById(id);
-//     button.onclick = function() {
-//         quiz.guess(guess);
-//         populate();
-//     }
-// };
- 
-// //change this to show if they got it correct or not 
-// function questionCorrect() {
-//     //  if answer is correct,
-//     element.innerHTML = "Correct"
-//     //if answer is incorrect, display incorrect and take away time
-// }
-// // function showProgress() {
-// //     var currentQuestionNumber = quiz.questionIndex + 1;
-// //     var element = document.getElementById("progress");
-// //     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-// // };
 
  
-// //quiz questions
-// var questions = [
-//     new Question("Commonly used data types DO NOT include: ", ["A. Strings", "B. Booleans", "C. Alerts", "D.Numbers"], "B. Booleans"),
-//     new Question("The condition in an if/else statement is enclosed within ___.", ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"], "C. Parentheses"),
-//     new Question("Arrays in JavaScript can be used to store ___.", ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"], "D. All of the Above"),
-//     new Question("String values must be enclosed within ______ when being assigned to variables.", ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parantheses"], "C. Quotes"),
-//     new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["A. JavaScript", "B. Terminal/Bash", "C. For Loops", "D. Console Log"], "D. Console Log"),
-// ]
+//quiz questions
+var questions = [
+    new Question("Commonly used data types DO NOT include: ", ["A. Strings", "B. Booleans", "C. Alerts", "D.Numbers"], "B. Booleans"),
+    new Question("The condition in an if/else statement is enclosed within ___.", ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"], "C. Parentheses"),
+    new Question("Arrays in JavaScript can be used to store ___.", ["A. Numbers and Strings", "B. Other Arrays", "C. Booleans", "D. All of the Above"], "D. All of the Above"),
+    new Question("String values must be enclosed within ______ when being assigned to variables.", ["A. Commas", "B. Curly Brackets", "C. Quotes", "D. Parantheses"], "C. Quotes"),
+    new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["A. JavaScript", "B. Terminal/Bash", "C. For Loops", "D. Console Log"], "D. Console Log"),
+]
 
 
-// //create quiz
-// var quiz = new Quiz(questions);
+//create quiz
+var quiz = new Quiz(questions);
 
-// //display quiz
-// populate();
+//display quiz
+populate();
 
 
 
 // //on submit, show results
-
-
 // //function to star ttimer
-
 // //enter high scores
-
 // //high scores page 
-
-// startButton.addEventListener("click", startQuiz());
